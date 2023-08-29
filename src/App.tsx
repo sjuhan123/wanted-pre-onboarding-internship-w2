64c23deb2806c68b970ba3134063d1a10da4899a
sjuhan123 <sjuhan123@gmail.com>
2023-08-29 22:03:29 +0900

    FEAT: 페이지별 초기 컴포넌트 구현 및 Router 세팅
    - Main 페이지 초기 구현
    - Issues 페이지 초기 구현
    - IssueDetail 페이지 초기 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import IssueDetail from './pages/IssueDetail';
+import Issues from './pages/Issues';
+import Main from './pages/Main';
 
 function App() {
-  return <div className='App'>world</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Main />} />
+        <Route path='/issues' element={<Issues />} />
+        <Route path='/issues/:id' element={<IssueDetail />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;

b3364ca1f1b52a766951d4915985fb67e179d004
sjuhan123 <sjuhan123@gmail.com>
2023-08-30 16:24:05 +0900

    FEAT: Github API Issue Detail Response Type 선언

---
+++ src/types/issueDetail.ts
@@ -0,0 +1,86 @@
+interface User {
+  login: string;
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  gravatar_id: string;
+  url: string;
+  html_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  starred_url: string;
+  subscriptions_url: string;
+  organizations_url: string;
+  repos_url: string;
+  events_url: string;
+  received_events_url: string;
+  type: string;
+  site_admin: boolean;
+}
+
+interface Label {
+  id: number;
+  node_id: string;
+  url: string;
+  name: string;
+  description: string;
+  color: string;
+  default: boolean;
+}
+
+interface Milestone {
+  url: string;
+  html_url: string;
+  labels_url: string;
+  id: number;
+  node_id: string;
+  number: number;
+  state: string;
+  title: string;
+  description: string;
+  creator: User;
+  open_issues: number;
+  closed_issues: number;
+  created_at: string;
+  updated_at: string;
+  closed_at: string;
+  due_on: string;
+}
+
+interface PullRequest {
+  url: string;
+  html_url: string;
+  diff_url: string;
+  patch_url: string;
+}
+
+export interface IIssueDetail {
+  id: number;
+  node_id: string;
+  url: string;
+  repository_url: string;
+  labels_url: string;
+  comments_url: string;
+  events_url: string;
+  html_url: string;
+  number: number;
+  state: string;
+  title: string;
+  body: string;
+  user: User;
+  labels: Label[];
+  assignee: User;
+  assignees: User[];
+  milestone: Milestone;
+  locked: boolean;
+  active_lock_reason: string;
+  comments: number;
+  pull_request: PullRequest;
+  closed_at: string | null;
+  created_at: string;
+  updated_at: string;
+  closed_by: User;
+  author_association: string;
+  state_reason: string;
+}

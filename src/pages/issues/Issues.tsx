f803f256ed6da5094a6d58a53e0852cc0b9b74fc
sjuhan123 <sjuhan123@gmail.com>
2023-08-30 16:31:02 +0900

    FEAT: Issues 페이지 구현
    - 이슈 목록 가져오는 API 사용
    - 상태에 따른 라우팅 로직 구현
    - 비정상적인 상태일시 Main으로 이동하는 로직 구현
    - 로딩 UI/UX 구현

--- src/pages/issues/Issues.tsx
+++ src/pages/issues/Issues.tsx
@@ -1,5 +1,53 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Layout from '../../components/common/Layout/Layout';
+import Spinner from '../../components/common/spinner/Spinner';
+import IssueList from '../../components/issues/issueList/IssueList';
+import { URL } from '../../constants';
+import { useIssueListContext } from '../../context/IssueListContext';
+import { useRepoContext } from '../../context/RepoContext';
+import useOctokit from '../../hooks/useOctokit';
+import { TIssueList } from '../../types/issue';
 
 export default function Issues() {
-  return <div>Issues</div>;
+  const navigate = useNavigate();
+  const {
+    state: { organization, repository },
+  } = useRepoContext();
+  const { dispatch } = useIssueListContext();
+
+  const { data, status } = useOctokit<TIssueList>(
+    `/repos/${organization}/${repository}/issues?per_page=10&sort=comments`,
+    {
+      owner: organization,
+      repo: repository,
+      headers: {
+        'X-GitHub-Api-Version': '2022-11-28',
+      },
+    },
+    true,
+  );
+
+  useEffect(() => {
+    if (data && status === 'success') {
+      dispatch({ type: 'SET_ISSUE_LIST', payload: [...data] });
+    }
+  }, [data]);
+
+  useEffect(() => {
+    if (!organization || !repository) {
+      navigate(URL.Main);
+    }
+  }, [organization, repository]);
+
+  return (
+    <Layout
+      buttonName='홈'
+      buttonOnClick={() => navigate(URL.Main)}
+      title={`${organization}/${repository}`}
+      titleOnClick={() => window.location.reload()}
+    >
+      {status === 'loading' ? <Spinner type='page' /> : <IssueList />}
+    </Layout>
+  );
 }

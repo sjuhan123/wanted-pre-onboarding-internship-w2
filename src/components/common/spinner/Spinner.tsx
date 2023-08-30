92f4361db8474f6acad5399eadd7a207ca6ac875
sjuhan123 <sjuhan123@gmail.com>
2023-08-30 16:25:56 +0900

    FEAT: Spinner 컴포넌트 prop type 추가해서 스타일 다양성 추가

--- src/components/common/spinner/Spinner.tsx
+++ src/components/common/spinner/Spinner.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import * as S from './style';
 
-export default function Spinner() {
-  return <S.Spinner />;
+export default function Spinner({ type }: S.SpinnerProps) {
+  return type === 'page' ? (
+    <S.SpinnerBox>
+      <S.Spinner type={type} />
+    </S.SpinnerBox>
+  ) : (
+    <S.Spinner type={type} />
+  );
 }
--- src/components/common/spinner/style.ts
+++ src/components/common/spinner/style.ts
@@ -1,17 +1,28 @@
 import styled, { keyframes } from 'styled-components';
 
+export interface SpinnerProps {
+  type: 'button' | 'page';
+}
+
 export const spinAnimation = keyframes`
   0% { transform: rotate(0deg); }
   100% { transform: rotate(360deg); }
 `;
 
-export const Spinner = styled.div`
+export const Spinner = styled.div<SpinnerProps>`
   display: flex;
   box-sizing: border-box;
-  width: 13px;
-  height: 13px;
-  border: 3px solid #fff;
+  width: ${({ type }) => (type === 'page' ? '30px' : '13px')};
+  height: ${({ type }) => (type === 'page' ? '30px' : '13px')};
+  border: 3px solid ${({ type }) => (type === 'page' ? '#000' : '#fff')};
   border-top-color: transparent;
   border-radius: 50%;
   animation: ${spinAnimation} 1s linear infinite;
+`;
+
+export const SpinnerBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 10vh;
 `;

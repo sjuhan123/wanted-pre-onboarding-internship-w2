376375e5c1b6ee4df13bb26fd8debf962fc85359
sjuhan123 <sjuhan123@gmail.com>
2023-09-01 21:11:37 +0900

    FIX: IssueList provider 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { PATH } from './constants';
-import { IssueListProvider } from './context/IssueListContext';
 import { RepoProvider } from './context/RepoContext';
 import IssueDetail from './pages/issueDetail/IssueDetail';
 import Issues from './pages/issues/Issues';
@@ -13,14 +12,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path={PATH.MAIN} element={<Main />} />
-          <Route
-            path={PATH.ISSUES}
-            element={
-              <IssueListProvider>
-                <Issues />
-              </IssueListProvider>
-            }
-          />
+          <Route path={PATH.ISSUES} element={<Issues />} />
           <Route path={PATH.ISSUE_DETAIL} element={<IssueDetail />} />
         </Routes>
       </BrowserRouter>
